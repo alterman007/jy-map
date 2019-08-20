b3ba3030d6c8fb60c3dae68d6e118f22ba514b97
alterman <caodx@citydo.com.cn>
2019-08-20 13:34:40 +0800

    添加线

--- src/containers/Map/Area.js
+++ src/containers/Map/Area.js
@@ -1,29 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { GeoJSON } from 'react-leaflet';
 import * as turf from '@turf/turf';
 import areaData from './area.json';
 
-
+const lineData = [
+  [
+    121.32313728332518,
+    31.285225971149053
+  ],
+  [
+    121.33747100830077,
+    31.26255800928356
+  ],
+  [
+    121.33060455322266,
+    31.255807919878652
+  ]
+];
 class Path extends Component {
-  geojsonStyle = {
+  geojsonStyleArea = {
     color: '#20AAFF',
     weight: 4,
     fillOpacity: 0.5,
     fillColor: '#20AAFF',
   };
 
-  state = {
-    areaData: turf.polygon([areaData]),
-  }
-
+  geojsonStyleLine = {
+    color: 'red',
+    weight: 4,
+  };
 
   render() {
-    const { areaData } = this.state;
     return (
-      <GeoJSON
-        data={turf.featureCollection([areaData])}
-        style={this.geojsonStyle}
-      />
+      <Fragment>
+        <GeoJSON
+          data={turf.featureCollection([turf.polygon([areaData])])}
+          style={this.geojsonStyleArea}
+        />
+        <GeoJSON
+          data={turf.featureCollection([turf.lineString(lineData)])}
+          style={this.geojsonStyleLine}
+        />
+      </Fragment>
     );
   }
 }
